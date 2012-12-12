10feeb8b35ca507131eb6ca0037ed474ca8fd4c3
selftext <jsalfen@gmail.com>
2012-12-12 18:48:17 -0500

    settimeout looping in time.js

--- js/time.js
+++ js/time.js
@@ -13,14 +13,16 @@ $.extend(true,WSR,{
 var Timer = (function($,_,d3){	// is "Time" a library name in JS? completely blanking...
 	return function(){
 
-		// temporarily storing array of current airports here
-		var currentAirports = [];
+		var currentAirports = [],
+			interval = 1000,
+			playing = true;
 		
 		// other function stuff goes here
 			
 			// increment time - range: 1999_1 to 2012_7
 			incrementTime = function (data) {
-				var months = ['1999_11','1999_12','2000_1']; // temporary - ulimately will get this from the data parameter
+				//var months = ['1999_11','1999_12','2000_1'];
+				var months = data;
 				months = _.map(months, function(m) {
 					if (m != '2012_7') {
 						mSplit = m.split('_');
@@ -28,15 +30,18 @@ var Timer = (function($,_,d3){	// is "Time" a library name in JS? completely bla
 						if (mSplit[1] != '12') m = mSplit[0] + '_' + ++mSplit[1];
 						// increment year and set month to january
 						else m = ++mSplit[0] + '_' + '1';
+					} else {
+						// switch back to first - DO THIS!
 					}
 					return m;
 				});
+				fetchIncidents(months);
 			} // END incrementTime
 
 			// load new incident files
 			fetchIncidents = function (data) {
 				// temporary - ulimately will get this from the data parameter
-				var months = ['1999_10','1999_11'];
+				var months = data;
 				WSR.vars.incidents = {};
 				// for each month, load the incidents file
 				_.each(months, function(m,i) {
@@ -49,11 +54,15 @@ var Timer = (function($,_,d3){	// is "Time" a library name in JS? completely bla
 							WSR.vars.incidents[m] = data;
 							// merge airports into current airports, removing any duplicates
 							currentAirports = _.union(currentAirports, fetchAirports(data));
-							// check for final iteration, then update map and global date variable
+							// check for final iteration, then update map and global variables
 							if (i == months.length - 1) {
 								updateMap(currentAirports);
 								WSR.vars.date = months;
-								// check if playing, then call setTimeout(incrementTime)
+								WSR.vars.airports = currentAirports;
+								// if playing, call the settimeout loop
+								if (playing) {
+									setTimeout(incrementTime(months),interval);
+								};
 							};
 						},
 						error: function(jqXHR, textStatus, errorThrown){
